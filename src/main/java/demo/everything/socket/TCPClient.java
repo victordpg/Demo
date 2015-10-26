60f4464a45ea2923b5ee6a32ff34f61fa6bff054
Victor <diaopg@sina.cn>
2015-10-26 09:38:24 +0800

    update

--- src/main/java/demo/everything/socket/TCPClient.java
+++ src/main/java/demo/everything/socket/TCPClient.java
@@ -1,10 +1,8 @@
 package demo.everything.socket;
 
 import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
