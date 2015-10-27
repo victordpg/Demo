9eb831ea26a845476a27121fa4d76013687733ea
Victor <diaopg@sina.cn>
2015-10-27 13:32:35 +0800

    add a sql

--- src/main/java/sql/postgresql.sql
+++ src/main/java/sql/postgresql.sql
@@ -1,7 +1,6 @@
 -- 1. json字段查询，排序
 -- 查询排序
-SELECT
-		device_type,
+SELECT  device_type,
 	metric_code,
 	metric_name,to_number(
 		metric_info ->> 'byteIndex',
@@ -30,9 +29,42 @@ ORDER BY metric_code,
 select * from aplus_device where conn_info->0->>'host' = '10.1.5.197';
 -- 查询json中的int
 select * from aplus_device where to_number(conn_info->0->>'pn','9') = 2;	
-	
+
 
 -- 2. 利用字符串操作函数，获得指定字符串
 -- 字段action_info原始值: {"byteCount":"1","byteIndex":"0","unit":"","timeUnit":"SECONDS","interval":"0","byteFormat":"BYTE_ARRAY2INT"}
 select substring(action_info from '\"byteIndex\"\:\"...') byteIndex from aplus_action where action_code = '04_F103' and device_type='SZPDZD';
 -- 查询后得到的结果："byteIndex":"0",
+
+
+-- 3. 字符串操作函数
+-- 字段action_info原始值：{"byteCount":"1","byteIndex":"0","unit":"","timeUnit":"SECONDS","interval":"0","byteFormat":"BYTE_ARRAY2INT"}
+-- 查询后byteIndex结果：0
+SELECT
+  action_name,
+	SUBSTRING (
+		SUBSTRING (
+			action_info
+			FROM
+				'\"byteIndex\"\:\"...'
+		),
+		'([0-9]|[0-9][0-9])'
+	) byteIndex
+FROM
+	aplus_action
+WHERE
+	action_code = '04_F21'
+AND device_type = 'SZPDZD'
+ORDER BY
+	to_number(
+		SUBSTRING (
+			SUBSTRING (
+				action_info
+				FROM
+					'\"byteIndex\"\:\"...'
+			),
+			'([0-9]|[0-9][0-9]|[0-9][0-9])'
+		),
+		'99'
+	);
+
