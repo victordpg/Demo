9b6af37aac1bc115c5b83f2efdff811c8d4c87f5
Victor <diaopg@sina.cn>
2016-02-22 14:09:15 +0800

    update disruptor constructor to version 3.x.x

--- src/main/java/demo/everything/disruptor/LongEntityMain.java
+++ src/main/java/demo/everything/disruptor/LongEntityMain.java
@@ -1,7 +1,7 @@
 package demo.everything.disruptor;
 
-import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 
 import com.lmax.disruptor.RingBuffer;
 import com.lmax.disruptor.dsl.Disruptor;
@@ -11,10 +11,13 @@ public class LongEntityMain {
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		Executor executor = Executors.newCachedThreadPool();
+		//Executor executor = Executors.newCachedThreadPool();
+		ThreadFactory threadFactory = Executors.defaultThreadFactory();
 		LongEntityFactory factory = new LongEntityFactory();
 		int bufferSize = 1024;
-		Disruptor<LongEntity> disruptor = new Disruptor<LongEntity>(factory, bufferSize, executor);
+		//Disruptor<LongEntity> disruptor = new Disruptor<LongEntity>(factory, bufferSize, executor);
+		//using following constructor to create new Disruptor since disruptor 3.x.x 
+		Disruptor<LongEntity> disruptor = new Disruptor<LongEntity>(factory, bufferSize, threadFactory);
 		disruptor.handleEventsWith(new LongEntityHandler());
 		disruptor.start();
 		
@@ -25,7 +28,7 @@ public class LongEntityMain {
         {
 			if (l<25) {
 	            producer.onData(l);
-	            Thread.sleep(100);
+	            //Thread.sleep(100);
 			} else {
 				break;
 			}
